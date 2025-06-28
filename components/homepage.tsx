6b4ff77134c40cba72f6f4e6f23b3fe00a4da19b
marialoudiaz <loumariadiaz@gmail.com>
2025-06-28 19:05:13 +1000

    css

--- components/homepage.tsx
+++ components/homepage.tsx
@@ -6,11 +6,10 @@ import '../styles/App.scss';
 const Main = () => {
 
   return (
-    <>
-    <video className='hp-video' autoPlay muted playsInline>
-        <source src="/video/georges-hp-lebon.mp4" type="video/mp4" />
-      </video>
-    </>
+    <video className="hp-video" autoPlay muted playsInline loop>
+      <source src="/video/georges-hp-lebon.mp4" type="video/mp4" />
+      Votre navigateur ne supporte pas la vidéo HTML5.
+    </video>
       
   );
 };
