7bcf0b62585ffd435efa1ba0d2980022a676e956
marialoudiaz <loumariadiaz@gmail.com>
2025-06-28 20:56:52 +1000

    css

--- components/contactForm.tsx
+++ components/contactForm.tsx
@@ -69,12 +69,36 @@ const ContactForm = () => {
           />
         </div>
 
-        <div className="form-button-wrapper wide">
-          <button className="form-button" type="submit" disabled={submitting}>
-            Envoyer →
+          <button className="button" type="submit" disabled={submitting}>
+            <span className="button__icon-wrapper">
+              <svg
+                viewBox="0 0 14 15"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                class="button__icon-svg"
+                width="10"
+              >
+                <path
+                  d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"
+                  fill="currentColor"
+                ></path>
+              </svg>
+              <svg
+                viewBox="0 0 14 15"
+                fill="none"
+                width="10"
+                xmlns="http://www.w3.org/2000/svg"
+                class="button__icon-svg button__icon-svg--copy"
+              >
+                <path
+                  d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"
+                  fill="currentColor"
+                ></path>
+              </svg>
+            </span>
+            Envoyer
           </button>
           {message && <p className="form-message">{message}</p>}
-        </div>
       </form>
     </div>
   );
