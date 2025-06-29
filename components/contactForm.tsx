fb6659ca4583cdab0154cbf380fca151cb4c822c
marialoudiaz <loumariadiaz@gmail.com>
2025-06-29 20:09:17 +1000

    css

--- components/contactForm.tsx
+++ components/contactForm.tsx
@@ -75,7 +75,7 @@ const ContactForm = () => {
                 viewBox="0 0 14 15"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
-                class="button__icon-svg"
+                className="button__icon-svg"
                 width="10"
               >
                 <path
@@ -88,7 +88,7 @@ const ContactForm = () => {
                 fill="none"
                 width="10"
                 xmlns="http://www.w3.org/2000/svg"
-                class="button__icon-svg button__icon-svg--copy"
+                className="button__icon-svg button__icon-svg--copy"
               >
                 <path
                   d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"
